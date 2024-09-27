b1b5e1f7ade0e1f3d69356295969dcc152fe4fef
Bianca Salvador <bbiancsalvador@gmail.com>
2024-09-27 09:28:30 +0800

    update the discussionh with Arithmetic Operators

---
+++ index.js
@@ -0,0 +1,34 @@
+// Arithmetic Operators
+
+	let x = 2003;
+	let y = 2002;
+
+	let sum = x + y;
+	console.log(`Result of Addition Op: ${sum}`);
+
+	let diff = x - y;
+	console.log(`Result of Subtraction Op: ${diff}`);
+
+	let prod = x * y;
+	console.log(`Result of Multiplication Op: ${prod}`);
+
+	let quotient = x / y;
+	console.log(`Result of Division Op: ${quotient}`);
+
+	let remainder = x % y;
+	console.log(`Result of Modulo Op: ${remainder}`);
+
+// Assignment Operator
+	//Basic Assignment Operator (=) -> the assignment operator adds the value of the right operand to a variable and assigns the result to the variable
+
+	let assignmentNumber = 8;
+
+	// Addition Assignment Operator
+	// The addition assignment operator adds the value of the right operand to a variable and assigns the result to the variable
+
+	assignmentNumber = assignmentNumber + 2;
+	console.log(`Result of addition assignment operator: ${assignmentNumber}`)
+	
+	// shorthand
+	assignmentNumber += 2;
+	console.log(`Result of Addition Assignment Operator: ${assignmentNumber}`)
