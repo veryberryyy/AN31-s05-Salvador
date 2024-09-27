1f72ca41d448bbbe1f085ed147119c9af114098d
Bianca Salvador <bbiancsalvador@gmail.com>
2024-09-27 09:44:26 +0800

    add discussion on Operators

--- index.js
+++ index.js
@@ -27,8 +27,37 @@
 	// The addition assignment operator adds the value of the right operand to a variable and assigns the result to the variable
 
 	assignmentNumber = assignmentNumber + 2;
-	console.log(`Result of addition assignment operator: ${assignmentNumber}`)
+	console.log(`Result of addition assignment operator: ${assignmentNumber}`);
 	
 	// shorthand
 	assignmentNumber += 2;
-	console.log(`Result of Addition Assignment Operator: ${assignmentNumber}`)
+	console.log(`Result of Addition Assignment Operator: ${assignmentNumber}`);
+
+	// subraction -=
+	// multiplication *=
+	//division /=
+
+	assignmentNumber -= 2;
+	console.log(`Result of Subtraction Assignment Operator: ${assignmentNumber}`);
+
+	assignmentNumber *= 2;
+	console.log(`Result of Multiplication Assignment Operator: ${assignmentNumber}`);
+
+	assignmentNumber /= 2;
+	console.log(`Result of Division Assignment Operator: ${assignmentNumber}`);
+
+// Multiple Operators and Parenthesis
+/*
+	- when multiple operators are applied in a single statement, it follows the PEMDAS
+	- the operations were done in the following:
+		ex. 1 + 2 - 3 * 4 / 5
+		1. 3 * 4 = 12
+		2. 12 / 5 = 2.4
+		3. 1 + 2 = 3
+		4. 3 - 2.4 = 0.6
+*/
+
+	let mdas = 1 + 2 - 3 * 4 / 5;
+		console.log(`Result of MDAS Operator: ${mdas}`);
+
+	
